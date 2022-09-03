ee81757e3b88c5d5096db4f6d6e53ae1d62a6de0
Rievvy <thiagoalmeidadepaiva@gmail.com>
2022-09-02 22:42:37 -0300

    Adicionando o Device Preview

--- flutter-project/lib/main.dart
+++ flutter-project/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/app.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const MyApp());
+  DevicePreview(
+    enabled: true,
+    tools: const [...DevicePreview.defaultTools],
+    builder: ((context) => (const MyApp())),
+  );
 }
