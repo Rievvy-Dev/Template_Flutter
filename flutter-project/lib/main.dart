0fde81a750f50fd2ea763719f6397ab92e74e4c3
Rievvy <thiagoalmeidadepaiva@gmail.com>
2022-09-06 23:46:08 -0300

    Alterações no main

--- flutter-project/lib/main.dart
+++ flutter-project/lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_project/app.dart';
-import 'package:device_preview/device_preview.dart';
 
 void main() {
-  DevicePreview(
+  runApp(DevicePreview(
     enabled: true,
-    tools: const [...DevicePreview.defaultTools],
-    builder: ((context) => (const MyApp())),
-  );
+    tools: [
+      ...DevicePreview.defaultTools,
+    ],
+    builder: (context) => const MyApp(),
+  ));
 }
